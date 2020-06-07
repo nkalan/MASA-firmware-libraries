905290e899e87030ba82d41b55125c6a4b210a94
Arthur <arthurzh@umich.edu>
2020-06-07 11:35:57 -0700

    added documentation for adc usage

--- MAX11131/adc.c
+++ MAX11131/adc.c
@@ -12,6 +12,7 @@ void init_adc(SPI_HandleTypeDef* SPI_BUS, GPIO_ADC_Pinfo *pins, int num_adcs) {
     uint8_t adcn = 0;
     // Copy all adc CS addresses
     uint16_t adc_cmd    = 0;
+    uint16_t adc_pin, adc_port = 0;
     for(int i = 0; i < num_adcs; ++i) {
         // Configure each ADC identically
         adc_pin = pinfo.ADC_CS_ADDR[i];
@@ -31,30 +32,28 @@ void init_adc(SPI_HandleTypeDef* SPI_BUS, GPIO_ADC_Pinfo *pins, int num_adcs) {
     __enable_irq();
 }
 
-uint16_t* read_adc(SPI_HandleTypeDef* SPI_BUS, uint8_t adcn) {
-    // Reads for entire FIFO register on ADC
-    uint16_t full_rx[16];
-
-    // start ADC from correct base addr
-    adcn    = ADC0 + adcn;
+uint16_t* read_adc_range(SPI_HandleTypeDef* SPI_BUS, uint8_t adcn, uint8_t *channels, uint8_t ch_num) {
+    uint16_t full_rx[16] = {-1};
 
     __disable_irq();
     // ~CS should be high, Set ~CNVST low before pulling high after 5ns
     HAL_GPIO_WritePin(  pinfo.ADC_CNVST_PORT[adcn], 
                         pinfo.ADC_CNVST_ADDR[adcn], 
-                        GPIO_PIN_SET);
+                        GPIO_PIN_SET
+                        );
     // Wait for EOC to pull low, then set ~CS low 
     while(HAL_GPIO_ReadPin(pinfo.ADC_EOC_PORT[adcn], pinfo.ADC_EOC_ADDR[adcn])){}
     set_adc(pinfo.ADC_CS_PORT[adcn], pinfo.ADC_CS_ADDR[adcn], GPIO_PIN_RESET);
 
     uint8_t tx[2];
     uint8_t rx[2];
-    for(uint8_t ch = 0; ch < 16; ++ch) {
+    for(uint8_t i = 0; i < ch_num; ++i) {
+        uint8_t ch  = channels[i];
         //0b00001 sets that next channel to be selected is identified
-		// in each SPI frame, conversion results are sent out next frame
-		tx[0] = (ch >> 1) | 0b00000000; // gets 3 MSB from channel num
-		tx[1] = (ch << 7) | 0b00000000; // gets LSB from channel num
-        // Read analog in data from FIFO channels
+        // in each SPI frame, conversion results are sent out next frame
+        tx[0] = (ch >> 1) | 0b00000000; // gets 3 MSB from channel num
+        tx[1] = (ch << 7) | 0b00000000; // gets LSB from channel num
+        // Read analog in data from FIFO channel
         read_adc_ch(SPI_BUS, &tx, &rx);
         full_rx[ch] = (rx[1] | (rx[0]<<8)) & 0x0FFF; // MSB 4 bits is simply ch addr, LSB 12 bits is adc data
     }
@@ -68,10 +67,58 @@ uint16_t* read_adc(SPI_HandleTypeDef* SPI_BUS, uint8_t adcn) {
                         GPIO_PIN_RESET);
 
     // Reenable Interrupts
-    __disable_irq();
+    __enable_irq();
 
     // return adc data from all channels
-    return &full_rx;
+    return full_rx;
+}
+
+
+void set_read_adc_range(SPI_HandleTypeDef* SPI_BUS, uint8_t adcn, uint8_t *channels, uint8_t ch_num) {
+    uint8_t tx[2];
+	uint8_t rx[2];
+
+    /* Set ADC to custom scan channel range */
+    __disable_irq();
+
+    /* ~CS should be high, Set ~CNVST low before pulling high after 5ns */
+    uint16_t SET_CUSTOM_SCAN        = ADC_MODE_CNTL|SCAN_STATES.CUSTOM_INT;
+    
+    uint16_t SET_SCAN_REGISTER_0    = ADC_CUSTOM_SCAN0;
+    uint16_t SET_SCAN_REGISTER_1    = ADC_CUSTOM_SCAN1;
+    for (uint8_t i = 0; i < ch_num; ++i) {
+        uint8_t ch = channels[i];
+        if (ch > 7) {
+            ch -= ADC_CUSTOM_SCAN1_SUB;
+            SET_SCAN_REGISTER_1 = SET_SCAN_REGISTER_1|(1<<ch);
+        } else {
+            ch += ADC_CUSTOM_SCAN0_ADD;
+            SET_SCAN_REGISTER_0 = SET_SCAN_REGISTER_0|(1<<ch);
+        } // sets channel register bit in Custom Scan0 Register
+    }
+
+    /* Configure ADC with channels */
+    uint16_t adc_pin    = pinfo.ADC_CS_ADDR[adcn];
+    uint16_t adc_port   = pinfo.ADC_CS_PORT[adcn];
+
+    /* Transmit custom channels to send data from */
+    set_adc(adc_pin, adc_port, GPIO_PIN_RESET); // Select adc
+    package_cmd(SET_SCAN_REGISTER_1, tx);
+    write_adc_reg(SPI_BUS, tx, rx, 2); // Select register of adc
+    package_cmd(SET_SCAN_REGISTER_0, tx);
+    write_adc_reg(SPI_BUS, tx, rx, 2); // Select register of adc
+    set_adc(adc_pin, adc_port, GPIO_PIN_SET); // Release adc
+
+    __enable_irq();
+}
+
+uint16_t* read_adc_all(SPI_HandleTypeDef* SPI_BUS, uint8_t adcn) {
+    // Convenience function for reading all channels on adc
+    uint8_t channels[16] = {0, 1, 2, 3, 4, 5, 6, 7, 8
+                            9, 10, 11, 12, 13, 14, 15} 
+    uint8_t ch_num      = 16;
+    return read_adc_range(SPI_BUS, adcn, channels, ch_num);
+    
 }
 
 void read_adc_ch(SPI_HandleTypeDef* SPI_BUS, uint8_t *tx, uint8_t *rx) {
--- MAX11131/adc.h
+++ MAX11131/adc.h
@@ -15,24 +15,30 @@
 #define ADC0                190 // all subsequent adcs increment from base by 1
 
 // Register Identification Code
-//
 // datasheet definitions between on pg21
-#define ADC_MODE_CNTL   0x0000; // 0b0          followed by 0s
-#define ADC_CONFIG      0x8000; // 0b1000       followed by 0s
-#define ADC_UNIPOLAR    0x8800; // 0b10001000   followed by 0s
-#define ADC_BIPOLAR     0x9000; // 0b1001       followed by 0s
-#define ADC_RANGE       0x9800; // 0b10011000   followed by 0s
+#define ADC_MODE_CNTL               0x0000; // 0b0          followed by 0s
+#define ADC_CONFIG                  0x8000; // 0b1000       followed by 0s
+#define ADC_UNIPOLAR                0x8800; // 0b10001000   followed by 0s
+#define ADC_BIPOLAR                 0x9000; // 0b1001       followed by 0s
+#define ADC_RANGE                   0x9800; // 0b10011000   followed by 0
+
+/* Offsets base channel addr to modify correct scan register bit */
+#define ADC_CUSTOM_SCAN0_ADD        0x0003; // 0b11         preceded by 1s
+#define ADC_CUSTOM_SCAN1_SUB        0x0005; // 0b101        preceded by 1s
+/* Register bits used to configure ADC scan registers */
+#define ADC_CUSTOM_SCAN0            0xA000; // 0b10100      followed by 0s
+#define ADC_CUSTOM_SCAN1            0xA800; // 0b10101      followed by 0s
 
 /* Global Var Definitions */
 // TODO: document what hardware setup needs to be for this library
 //          Write up README talking about what 
 typedef GPIO_ADC_Pinfo {
-    int ADC_CS_ADDR[8];
-    int ADC_CS_PORT[8];
-    int ADC_EOC_ADDR[8];
-    int ADC_EOC_PORT[8];
-    int ADC_CNVST_ADDR[8];
-    int ADC_CNVST_PORT[8];
+    uint16_t ADC_CS_ADDR[8];
+    uint16_t ADC_CS_PORT[8];
+    uint16_t ADC_EOC_ADDR[8];
+    uint16_t ADC_EOC_PORT[8];
+    uint16_t ADC_CNVST_ADDR[8];
+    uint16_t ADC_CNVST_PORT[8];
 } GPIO_ADC_Pinfo;
 
 GPIO_ADC_Pinfo pinfo;
@@ -45,7 +51,9 @@ enum SCAN_STATES{
     STD_INT,
     STD_EXT,
     UPPER_INT,
-    UPPER_EXT
+    UPPER_EXT,
+    CUSTOM_INT,
+    CUSTOM_EXT
 }
 
 /* Public Function Prototypes */
@@ -60,27 +68,52 @@ enum SCAN_STATES{
  * 
  *  Note: assumes 8bit data framing on SPI
  */
-void init_adc(SPI_HandleTypeDef* SPI_BUS, GPIO_ADC_Pinfo *pinfo, int num_adcs);
+void init_adc(SPI_HandleTypeDef* SPI_BUS, GPIO_ADC_Pinfo *pinfo, uint8_t num_adcs);
 
 /**
- *  Reads adc range from a specified range of pins on adc
+ *  Reads adc range from a specified range of pins on adc and returns arr of read values
  *  
  *  general documentation starts on datasheet pg 21
  *  @param SPI_BUS      <SPI_HandleTypeDef*> SPI object ADC is on
  *  @param adc_num      <uint8_t> selected adc number
- *  @param ch_start     <uint8_t> start adc channel to read from, indexed from 0
- *  @param ch_num       <uint8_t> number of channels to read including ch_start
  * 
  */
-void read_adc_range(SPI_HandleTypeDef* SPI_BUS, uint8_t adc_num, uint8_t ch_start, uint8_t ch_num);
+uint16_t* read_adc_range(SPI_HandleTypeDef* SPI_BUS, uint8_t adcn);
+
+/**
+ *  Sets range to read from adc
+ *  
+ *  general documentation starts on datasheet pg 21
+ *  @param SPI_BUS      <SPI_HandleTypeDef*> SPI object ADC is on
+ *  @param adcn         <uint8_t> selected adc number
+ *  @param channels     <uint8_t*> arr of adc channels to read from
+ *  @param ch_num       <uint8_t> number of channels to read
+ * 
+ */
+void set_read_adc_range(SPI_HandleTypeDef* SPI_BUS, uint8_t adcn, uint8_t *channels, uint8_t ch_num);
 
 /* Private Helper Functions */
 
+/**
+ *  Convenience function for reading from all channels on ADCx
+ *  Note: this function assumes adc range to read from has already
+ *          been set by set_read_adc_range()
+ *  
+ *  general documentation starts on datasheet pg 21
+ *  @param SPI_BUS      <SPI_HandleTypeDef*> SPI object ADC is on
+ *  @param adcn         <uint8_t> selected adc number
+ * 
+ */
 uint16_t* read_adc(SPI_HandleTypeDef* SPI_BUS, uint8_t adcn);
+
+/**
+ *  
+ */
 void set_adc(uint8_t adcn, GPIO_PinState state);
 void write_adc_reg(SPI_HandleTypeDef* SPI_BUS, uint8_t *tx, uint8_t *rx);
 void read_adc_ch(SPI_HandleTypeDef* SPI_BUS, uint8_t *tx, uint8_t *rx);
 void package_cmd(uint16_t cmd, uint8_t* tx);
+
 
 
 #endif /* end header include protection */
