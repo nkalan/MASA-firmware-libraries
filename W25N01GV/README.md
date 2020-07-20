b1883906edc66b5b8ef47174f9ad4749b10145ef
nkalan <nkalan@umich.edu>
2020-07-20 15:17:23 -0400

    Initial commit

---
+++ W25N01GV/README.md
@@ -0,0 +1,6 @@
+# W25N01GV Flash Memory
+This repository contains the MASA firmware library for the W25N01GV flash memory IC
+
+
+# Implementing W25N01GV Firmware
+`TODO: write guide on implementing firmware`
---
+++ W25N01GV/src/W25N01GV.c
@@ -0,0 +1,9 @@
+/**
+ * Code implementation for communicating with W25N01GV Flash Memory
+ * Datasheet: https://www.winbond.com/resource-files/w25n01gv%20revl%20050918%20unsecured.pdf
+ * 
+ * Nathaniel Kalantar (nkalan@umich.edu)
+ * Michigan Aeronautical Science Association
+ * Created July 20, 2020
+ * Last edited July 20, 2020
+ */
