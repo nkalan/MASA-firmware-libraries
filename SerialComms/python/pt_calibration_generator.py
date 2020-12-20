941f1b0ba549813e3821d6d96fbd1060694272db
Leif Gullstad <leifg@umich.edu>
2020-12-20 11:39:44 -0700

    Create pt_calibration_generator.py

---
+++ SerialComms/python/pt_calibration_generator.py
@@ -0,0 +1,10 @@
+"""
+MASA pressure transducer format and global variables generator script
+
+Michigan Aeronautical Science Association
+Author: Leif Gullstad (leifg@umich.edu)
+Created: December 20, 2020
+"""
+import time
+import sys
+import csv
