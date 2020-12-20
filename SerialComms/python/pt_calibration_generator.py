2f86c5fd0d2ac775fda37de6404beaf09a571125
Leif Gullstad <leifg@umich.edu>
2020-12-20 11:51:00 -0700

    Update pt_calibration_generator.py

--- SerialComms/python/pt_calibration_generator.py
+++ SerialComms/python/pt_calibration_generator.py
@@ -3,8 +3,10 @@ MASA pressure transducer format and global variables generator script
 
 Michigan Aeronautical Science Association
 Author: Leif Gullstad (leifg@umich.edu)
+Modified from telem_file_generator.py by Nathaniel Kalantar (nkalan@umich.edu)
 Created: December 20, 2020
 """
 import time
 import sys
 import csv
+
