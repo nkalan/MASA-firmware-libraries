0264027947fbe821f370058d08fb920afda51a30
lowdrant <lmanderson42@gmail.com>
2021-02-19 09:01:56 -0500

    Major h-file bug fix; added overwrite flag.
    Bug fix
    -------
    - Originally had '#define CODEHASH <raw hassh text>'
    - Now is '#define CODEHASH "<raw hash text>"'
    - C doesn't know how to interpret a raw hash sequence, so it needs to be specified as a string
    
    Overwrite flag
    --------------
    - Allows user to check hash without updating codehash.h
    - Makes script more useful for code tracing
    
    Other
    -----
    Improved help string, made some potential errors more informative.

--- hashcode.py
+++ hashcode.py
@@ -1,29 +1,33 @@
 #!/usr/bin/env python3
 """
-CLI to develop firmware code hash. 
+CLI to track firmware using hash. 
 
 Recurses through a Core/ directory, aggregating text from files ending in .c
 and .h, hasing aggregate text and writing to Inc/codehash.h.
 """
 from argparse import ArgumentParser
 from datetime import datetime
-from hashlib import md5  # replace the __hash__, which is bad
+from hashlib import md5  # replace __hash__, which is bad
 from pathlib import Path
 
-parser = ArgumentParser(description='Hash firmware project and save in codehash.h.')
+parser = ArgumentParser(description='Determine firmware project hash. Does not automatically save to codehash.h')
 parser.add_argument('corepath', default=None, nargs='?', help="""
-    path to \'Core\' firmware directory. Defaults two directories up.
-    """)
+    path to project \'Core\' directory [default: firmware-libraries/../../]""")
+parser.add_argument('--overwrite', action='store_true',
+                    help='save hash to Core/Inc/codehash.h')
 args = parser.parse_args()
+path2script = Path(__file__).resolve().parent
+corepath = path2script.parent
 
 if __name__ == '__main__':
-    path2script = Path(__file__).resolve().parent
-    corepath = path2script.parent
     if args.corepath is not None:
         corepath = Path(args.corepath).resolve()
     if corepath.stem != 'Core':
         raise RuntimeError(f'not a Core/ path: \'{corepath}\'')
-    incpath = [v for v in corepath.iterdir() if 'inc' in str(v).lower()][0]
+    try:
+        incpath = [v for v in corepath.iterdir() if 'inc' in str(v).lower()][0]
+    except IndexError:
+        raise RuntimeError(f'Failed to find inc/ or Inc/ in \'{corepath}\'')
 
     # Recurse Through Files
     print(f'Aggregating code text in \'{corepath}\'')
@@ -41,15 +45,22 @@ if __name__ == '__main__':
     m = md5()
     m.update(bytes(str2hash, 'utf-8'))
     codehash = m.hexdigest()
-    fnhash = incpath / 'codehash.h'
-    writestr = f"""/** codehash.h
+    print(f'Current hash: {codehash}')
+    
+    # Overwrite inc/codehash.h
+    if args.overwrite:
+        fnhash = incpath / 'codehash.h'
+        writestr = f"""/** codehash.h
  *
  * Last Updated: {datetime.now().isoformat()}
  *
  * Firmware hash for this firmware project
  */
-#define CODEHASH {codehash}"""
-    with open(str(fnhash), 'w') as f:
-        f.write(writestr)
-    print(f'hash: {codehash}')
-    
+#ifndef CODEHASH_H
+#define CODEHASH_H
+#define CODEHASH \"{codehash}\"
+#endif
+"""
+        with open(str(fnhash), 'w') as f:
+            f.write(writestr)
+        print(f'Updated \'{incpath}/codehash.h\'')
