3cbda2d75874da4cf98dee389fd61e548a9fc4c7
Elham Islam <eislam@umich.edu>
2021-07-05 17:47:16 -0400

    add comments

--- W25N01GV/inc/valvelib.h
+++ W25N01GV/inc/valvelib.h
@@ -1,8 +1,10 @@
 /*
- * valvelib.h
+ * Header file for wrapper around existing valve functions
  *
- *  Created on: Jul. 5, 2021
- *      Author: elham
+ * Elham Islam (eislam@umich.edu)
+ * Michigan Aeronautical Science Association
+ * Created July 5, 2021
+ * Last edited July 5, 2021
  */
 
 #ifndef W25N01GV_INC_VALVELIB_H_ // Begin header include protection
@@ -11,15 +13,15 @@
 
 typedef struct {
 	uint8_t num_valves;
-	uint8_t valve_states[num_valves];
-	void (*set_valve_func)(uint8_t, uint8_t);
+	uint8_t valve_states[num_valves]; // array containing states of each valve
+	void (*set_valve_func)(uint8_t, uint8_t); // existing function used to set valve states
 
 
 } Valves;
 
-void power_valve(Valves *valves);
+void power_valve(Valves *valves); //set valve channels high
 
-void depower_valve(Valves *valves);
+void depower_valve(Valves *valves); //set valve channels low
 
 
 #endif /* W25N01GV_INC_VALVELIB_H_ */
--- W25N01GV/src/valvelib.c
+++ W25N01GV/src/valvelib.c
@@ -1,8 +1,9 @@
 /*
- * valvelib.c
- *
- *  Created on: Jul. 5, 2021
- *      Author: elham
+ * Implementation of wrapper around existing valve functions
+ * Elham Islam (eislam@umich.edu)
+ * Michigan Aeronautical Science Association
+ * Created July 5, 2021
+ * Last edited July 5, 2021
  */
 
 
@@ -12,14 +13,14 @@
 void power_valve(Valves *valves){
 	for(i = 0; i < valves->num_valves; i++){
 		if(valves->valve_states[i] == 1){
-		valves->set_valve_func(i, 1);
+		valves->set_valve_func(i, 1); // set valve channel to high as per valve_states array
 	}
 }
 
 void depower_valve(Valves *valves){
 	for(i = 0; i < valves->num_valves; i++){
 		if(valves->valve_states[i] == 0){
-		valves->set_valve_func(i, 0);
+		valves->set_valve_func(i, 0); // set valve channel to low as per valve_states array
 	}
 }
 
