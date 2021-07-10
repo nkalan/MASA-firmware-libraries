f522904500a5d0024dae26dd0e6599cd428c6387
samzliu <samzliu@umich.edu>
2021-07-09 21:24:28 -0400

    set type works, temperature reads 0

--- MAX31856/src/MAX31856.c
+++ MAX31856/src/MAX31856.c
@@ -26,7 +26,7 @@
 #define MAX31856_CR1_REG_Write (0x81)
 #define MAX31856_TCTYPE_T ((uint8_t) 0b0111)
 
-#define MAX31856_TIMEOUT (0xFF)
+#define MAX31856_TIMEOUT (0xff)
 // Add any private helper functions here as required
 
 
@@ -38,13 +38,15 @@ void MAX31856_init_thermocouples(MAX31856_TC_Array* tcs) {
 	// This might be the default, but check the datasheet.
 	// We want to be able to read it by only using chip select and MISO,
 	// and shouldn't need to transmit commands.
+
+	uint8_t reg_addr[1] = {MAX31856_CR1_REG_Read};
+	uint8_t type[1];
+	uint8_t tx[2];
+//	uint8_t check_type[1] = {0};
   
   for (uint8_t i = 0; i < tcs->num_tcs; i++) {
     // Read the type (default should be K) in CR1_REG
-    uint8_t reg_addr[1] = {MAX31856_CR1_REG_Read};
-    uint8_t type[1] = {0};
-    uint8_t check_type[1] = {0xff};
-
+	type[0] = 0;
     __disable_irq();
     (*tcs->chip_select)(i);
     HAL_SPI_Transmit(tcs->SPI_bus, (uint8_t *)reg_addr, 1, MAX31856_TIMEOUT);
@@ -57,22 +59,21 @@ void MAX31856_init_thermocouples(MAX31856_TC_Array* tcs) {
     type[0] |= MAX31856_TCTYPE_T;
     
     // Write the register
-    uint8_t tx[2] = {MAX31856_CR1_REG_Write, type[0]};
+    tx[0] = MAX31856_CR1_REG_Write;
+    tx[1] = type[0];
     __disable_irq();
     (*tcs->chip_select)(i);
-    HAL_SPI_Transmit(tcs->SPI_bus, (uint8_t *)&tx[0], 1, MAX31856_TIMEOUT);
-    HAL_SPI_Transmit(tcs->SPI_bus, (uint8_t *)&tx[1], 1, MAX31856_TIMEOUT);
+    HAL_SPI_Transmit(tcs->SPI_bus, (uint8_t *)tx, 2, MAX31856_TIMEOUT);
     (*tcs->chip_release)(i);
     __enable_irq();
 
-    // Checking the register
-    reg_addr[0] = 0x02;
-    __disable_irq();
-    (*tcs->chip_select)(i);
-    HAL_SPI_Transmit(tcs->SPI_bus, (uint8_t *)reg_addr, 1, MAX31856_TIMEOUT);
-    HAL_SPI_Receive(tcs->SPI_bus, (uint8_t *)check_type, 1, MAX31856_TIMEOUT);
-    (*tcs->chip_release)(i);
-    __enable_irq();
+//    // Checking the register
+//    __disable_irq();
+//    (*tcs->chip_select)(i);
+//    HAL_SPI_Transmit(tcs->SPI_bus, (uint8_t *)reg_addr, 1, MAX31856_TIMEOUT);
+//    HAL_SPI_Receive(tcs->SPI_bus, (uint8_t *)check_type, 1, MAX31856_TIMEOUT);
+//    (*tcs->chip_release)(i);
+//    __enable_irq();
   }
 }
 
@@ -80,7 +81,7 @@ float MAX31856_read_thermocouple(MAX31856_TC_Array* tcs, uint8_t tc_index) {
 	// Read the temperature value from a single thermocouple chip, specified by tc_index.
 	// Then convert that number into a real temp in Celcius, then convert that to Kelvin and return it.
   
-  uint8_t reg_addr[1] = {MAX31856_LNRZD_TC_TEMP_B0};
+  uint8_t reg_addr[1] = {MAX31856_LNRZD_TC_TEMP_B2};
   uint8_t rx[3] = { 0, 0, 0 };
   uint32_t temp32;
   float real_temp_c;
@@ -95,11 +96,11 @@ float MAX31856_read_thermocouple(MAX31856_TC_Array* tcs, uint8_t tc_index) {
   
   // Convert rx into real_temp
   temp32 = 0b00000000| rx[0] << 16 | rx[1] << 8 | rx[2];
-  if (temp32 & 0x800000) {
+  if (temp32 & 0x800000) { //if first bit is 1
       temp32 |= 0xFF000000; // fix sign
-    }
+  }
   temp32 >>= 5;
-  real_temp_c = temp32 / 128;
+  real_temp_c = temp32 / 128.0;
   
   // Convert from Celsius to Kelvin and return
   return real_temp_c + 273.15;
