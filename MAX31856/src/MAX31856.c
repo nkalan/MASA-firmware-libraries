50847587b6d9c69f20aec8462440db97043569bd
samzliu <samzliu@umich.edu>
2021-07-11 17:17:13 -0400

    TESTED added private helper functions

--- MAX31856/src/MAX31856.c
+++ MAX31856/src/MAX31856.c
@@ -31,9 +31,18 @@
 #define MAX31856_CR0_REG_Write (0x80)
 
 #define MAX31856_TIMEOUT (0x01)
-// Add any private helper functions here as required
 
+// Add any private helper functions here as required
 void MAX31856_Read_Register(MAX31856_TC_Array* tcs, int tc_index, uint8_t reg_addr, uint8_t * rx, int n) {
+	/*
+	 * Reads a register using SPI
+	 * Parameters:
+	 * tcs: array of thermocouples struct, each containing a SPI bus, chip_select, chip_release, and num_tcs
+	 * tc_index: index of the tc to read. This matters for chip_select and chip_release
+	 * reg_addr: which register to read
+	 * rx: buffer arry for receiving
+	 * n: how many bytes to receive
+	 */
 	uint8_t reg_buffer[1];
 	reg_buffer[0] = reg_addr;
     __disable_irq();
@@ -45,6 +54,16 @@ void MAX31856_Read_Register(MAX31856_TC_Array* tcs, int tc_index, uint8_t reg_ad
 }
 
 void MAX31856_Write_Register(MAX31856_TC_Array* tcs, int tc_index, uint8_t *tx, int n) {
+	/*
+	 * Writes a register using SPI
+	 * Parameters:
+	 * tcs: array of thermocouples struct, each containing a SPI bus, chip_select, chip_release, and num_tcs
+	 * tc_index: index of the tc to read. This matters for chip_select and chip_release
+	 * tx: buffer arry that gets transmitted.
+	 *     Note that the tx must be set before it is passed into this function.
+	 *     tx[0] is the register address to transmit into, followed by n bytes of data
+	 * n: how many bytes to write. Note that HAL_SPI_Transmit uses n + 1.
+	 */
     __disable_irq();
     (*tcs->chip_select)(tc_index);
     HAL_SPI_Transmit(tcs->SPI_bus, (uint8_t *)tx, n + 1, MAX31856_TIMEOUT);
@@ -62,21 +81,13 @@ void MAX31856_init_thermocouples(MAX31856_TC_Array* tcs) {
 	// We want to be able to read it by only using chip select and MISO,
 	// and shouldn't need to transmit commands.
 
-	uint8_t reg_addr[1];
 	uint8_t rx[1];
 	uint8_t tx[2];
 	uint8_t check[2];
   
   for (uint8_t i = 0; i < tcs->num_tcs; i++) {
     // Read the type (default should be K) in CR1_REG
-	reg_addr[0] = MAX31856_CR1_REG_Read;
 	rx[0] = 0;
-//    __disable_irq();
-//    (*tcs->chip_select)(i);
-//    HAL_SPI_Transmit(tcs->SPI_bus, (uint8_t *)reg_addr, 1, MAX31856_TIMEOUT);
-//    HAL_SPI_Receive(tcs->SPI_bus, (uint8_t *)rx, 1, MAX31856_TIMEOUT); //interrupt?
-//    (*tcs->chip_release)(i);
-//    __enable_irq();
     MAX31856_Read_Register(tcs, i, MAX31856_CR1_REG_Read, rx, 1);
 
     // Switch to type T
@@ -86,46 +97,26 @@ void MAX31856_init_thermocouples(MAX31856_TC_Array* tcs) {
     // Write the register
     tx[0] = MAX31856_CR1_REG_Write;
     tx[1] = rx[0];
-//    __disable_irq();
-//    (*tcs->chip_select)(i);
-//    HAL_SPI_Transmit(tcs->SPI_bus, (uint8_t *)tx, 2, MAX31856_TIMEOUT);
-//    (*tcs->chip_release)(i);
-//    __enable_irq();
     MAX31856_Write_Register(tcs, i, tx, 1);
 
-    // Checking the register
-    check[0] = 0;
-    check[1] = 0;
-//    __disable_irq();
-//    (*tcs->chip_select)(i);
-//    HAL_SPI_Transmit(tcs->SPI_bus, (uint8_t *)reg_addr, 1, MAX31856_TIMEOUT);
-//    HAL_SPI_Receive(tcs->SPI_bus, (uint8_t *)check, 2, MAX31856_TIMEOUT);
-//    (*tcs->chip_release)(i);
-//    __enable_irq();
-    MAX31856_Read_Register(tcs, i, MAX31856_CR0_REG_Read, check, 2);
+//    // Checking the register
+//    check[0] = 0;
+//    check[1] = 0;
+//    MAX31856_Read_Register(tcs, i, MAX31856_CR0_REG_Read, check, 2);
 
     // Change to automatic conversion every 100 ms (datasheet p19)
 	reg_addr[0] = MAX31856_CR0_REG_Read;
     rx[0] = 0;
-//    __disable_irq();
-//    (*tcs->chip_select)(i);
-//    HAL_SPI_Transmit(tcs->SPI_bus, (uint8_t *)reg_addr, 1, MAX31856_TIMEOUT);
-//    HAL_SPI_Receive(tcs->SPI_bus, (uint8_t *)rx, 1, MAX31856_TIMEOUT); //interrupt?
-//    (*tcs->chip_release)(i);
-//    __enable_irq();
+
+    // Changing to Automatic Conversion mode
     MAX31856_Read_Register(tcs, i, MAX31856_CR0_REG_Read, rx, 1);
 
-    // Change bit 7 to high, Automatic Conversion mode
+    // Set bit 7 to high (default should be 0, Normally Off)
     rx[0] |= 0b10000000;
 
     // Write the register CR0
     tx[0] = MAX31856_CR0_REG_Write;
     tx[1] = rx[0];
-//    __disable_irq();
-//    (*tcs->chip_select)(i);
-//    HAL_SPI_Transmit(tcs->SPI_bus, (uint8_t *)tx, 2, MAX31856_TIMEOUT);
-//    (*tcs->chip_release)(i);
-//    __enable_irq();
     MAX31856_Write_Register(tcs, i, tx, 1);
 
     // Checking the register
@@ -146,13 +137,7 @@ float MAX31856_read_thermocouple(MAX31856_TC_Array* tcs, uint8_t tc_index) {
   uint32_t temp32;
   float real_temp_c;
   
-  // Write into rx
-//  __disable_irq();
-//  (*tcs->chip_select)(tc_index);
-//  HAL_SPI_Transmit(tcs->SPI_bus, (uint8_t *)reg_addr, 1, MAX31856_TIMEOUT);
-//  HAL_SPI_Receive(tcs->SPI_bus, (uint8_t *)rx, 3, MAX31856_TIMEOUT); //DRDY?
-//  (*tcs->chip_release)(tc_index);
-//  __enable_irq();
+  // Receive into rx
   MAX31856_Read_Register(tcs, tc_index, MAX31856_LNRZD_TC_TEMP_B2, rx, 3);
   
   // Convert rx into real_temp
@@ -163,13 +148,16 @@ float MAX31856_read_thermocouple(MAX31856_TC_Array* tcs, uint8_t tc_index) {
       temp32 >>= 5; //last 5 bits are not used
       temp32 += 1;
 	  real_temp_c = temp32 / 128.0 * -1;
-	  real_temp_c = (int)(real_temp_c * 100 - 0.5) / 100; // 2 decimal places
+	  // 2 decimal places
+	  real_temp_c = (int)(real_temp_c * 100 - 0.5);
+	  real_temp_c /= 100;
   }
   else {
 	  temp32 >>= 5;
 	  real_temp_c = temp32 / 128.0;
+	  // 2 decimal places
 	  real_temp_c = (int)(real_temp_c * 100 + 0.5);
-	  real_temp_c /= 100;// 2 decimal places
+	  real_temp_c /= 100;
   }
   
   // Convert from Celsius to Kelvin and return
